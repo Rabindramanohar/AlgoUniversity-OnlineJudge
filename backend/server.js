aefeac5013c609efc7e5876ab44589f6f9da7110
rabindra <robinmanohar@gmail.com>
2023-10-19 23:54:48 +0530

    mern auth

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from 'dotenv';
+dotenv.config();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import userRoutes from './routes/userRoutes.js';
+const port = process.env.PORT || 5000;
+
+
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(cookieParser());
+
+app.use('/api/users', userRoutes);
+app.use('api/problems', )
+app.get('/', (req, res) => res.send(`server is ready!!`));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port || 8800, console.log(`server listening at port {8000}`));
